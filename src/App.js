87a10ef0cdf252422f5e6f5acb65ca5537c2b1a7
herzaparam <herzaparam@gmail.com>
2022-02-03 01:03:10 +0700

    first commit, setup boilerplate

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { hot } from 'react-hot-loader/root';
+
+function App() {
+  return (
+    <div>
+      <h1>hello this is from react</h1>
+    </div>
+  );
+}
+
+export default hot(App);
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const config = {
+  entry: ['react-hot-loader/patch', './src/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: './dist',
+    },
+    open: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
+
+module.exports = config;
